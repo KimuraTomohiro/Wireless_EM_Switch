e4be13e46d6c96aa170369d99ee4b19585166ab3
KIMURA TOMOHIRO <85008581+KimuraTomohiro@users.noreply.github.com>
2025-05-19 00:57:12 +0900

    add program(now editting)

---
+++ プログラム/Wireless_EM_Switch.cpp
@@ -0,0 +1,37 @@
+#include <TWELITE>
+
+const uint8_t LED_R = 11;
+const uint8_t LED_G = 13;
+const uint8_t EM_SW = 18;
+const uint8_t RESET_SW = 19;
+
+
+
+int iLedCounter = 0;
+
+/*** the setup procedure (called on boot) */
+void setup() {
+
+    pinMode(LED_R, OUTPUT);
+    pinMode(LED_G, OUTPUT);
+    pinMode(EM_SW,INPUT_PULLDOWN);
+
+    digitalWrite(LED_R, HIGH); // TURN LED_R OFF
+    digitalWrite(LED_G, LOW); // TURN LED_G ON
+
+    Serial << "--- act2 (using a timer) ---" << crlf;
+}
+
+/*** loop procedure (called every event) */
+void loop() {
+
+
+
+	if(digitalRead(EM_SW) == PIN_STATE::HIGH){
+		digitalWrite(LED_R, LOW);
+        digitalWrite(LED_G, HIGH);
+	}else{
+		digitalWrite(LED_R, HIGH);
+        digitalWrite(LED_G, LOW);
+	}
+}
